74903c844afede2f3aceb7aba81f8f8ff493a850
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-04-25 17:13:03 +0000

    A

---
+++ Contas.java
@@ -0,0 +1,21 @@
+public class Contas{
+
+    private int numero;
+    private double saldo;
+
+    public void setNumero(int numero){
+        this.numero=numero;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo=numero;
+    }
+
+    public double getSaldo(){
+        return this.saldo;
+    }
+    public int getNumero(){
+        return this.numero;
+    }
+
+}
---
+++ TesteContas.java
@@ -0,0 +1,10 @@
+public class TesteContas {
+
+    public static void main(String[] args){
+        Contas c= new Contas();
+        c.setNumero(122);
+        c.setSaldo(345);
+        System.out.println(c.getNumero());
+    }
+    
+}
